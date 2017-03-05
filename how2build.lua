c8c23f70ab6c0cf08d4916daff61dd15cb0ff9c8
Reed Weichler <rweichler@gmail.com>
2017-03-04 17:25:39 -0800

    emergency 4.7

--- how2build.lua
+++ how2build.lua
@@ -2,7 +2,7 @@ local deb = debber()
 deb.packageinfo = {
     Package = 'jjjj',
     Name = 'jjjj (Alpha)',
-    Version = '0.1~alpha4.6',
+    Version = '0.1~alpha4.7',
     Architecture = 'iphoneos-arm',
     Depends = 'firmware (>= 5.0), dpkg, bzip2, gzip, cydia, coreutils, luajit',--, ws.hbang.libopener',
     Depiction = 'http://cydia.r333d.com/view/jjjj',
